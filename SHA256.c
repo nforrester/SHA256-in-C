74b3bf8c70c7d62f43fb43335ccf7edbab46b90c
Neil Forrester <neil@nforrester.net>
2026-04-05 12:16:01 -0700

    remove trailing whitespace

--- SHA256.c
+++ SHA256.c
@@ -1,4 +1,4 @@
-// Author: Edward Eldridge 
+// Author: Edward Eldridge
 // Program: SHA-256 Algorithm implentation in C
 // Resources: https://github.com/EddieEldridge/SHA256-in-C/blob/master/README.md
 // Section Reference: https://nvlpubs.nist.gov/nistpubs/FIPS/NIST.FIPS.180-4.pdf
@@ -26,7 +26,7 @@ union messageBlock
 };
 
 // ENUM to control state of the program
-enum status{READ, 
+enum status{READ,
             PAD0,
             PAD1,
             FINISH
@@ -59,8 +59,8 @@ int nextMessageBlock(FILE *file, union messageBlock *msgBlock, enum status *stat
 
 
 // ==== Main ===
-int main(int argc, char *argv[]) 
-{   
+int main(int argc, char *argv[])
+{
      // Variables
     FILE *file;
     FILE *fileForPrinting;
@@ -117,14 +117,14 @@ int main(int argc, char *argv[])
         calculateHash(file, verbose);
 
     }
-    
+
     return 0;
 
 }
 
 // === Functions ===
 void calculateHash(FILE *file, bool verbose)
-{   
+{
     // Variables
     // The current message block
     union messageBlock msgBlock;
@@ -143,20 +143,20 @@ void calculateHash(FILE *file, bool verbose)
     // Defined in Section 4.2.2
     __uint32_t K[] =
     {
-        0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5, 
+        0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5,
         0x3956c25b, 0x59f111f1, 0x923f82a4, 0xab1c5ed5,
         0xd807aa98, 0x12835b01, 0x243185be, 0x550c7dc3,
         0x72be5d74, 0x80deb1fe, 0x9bdc06a7, 0xc19bf174,
         0xe49b69c1, 0xefbe4786, 0x0fc19dc6, 0x240ca1cc,
         0x2de92c6f, 0x4a7484aa, 0x5cb0a9dc, 0x76f988da,
-        0x983e5152, 0xa831c66d, 0xb00327c8, 0xbf597fc7, 
-        0xc6e00bf3, 0xd5a79147, 0x06ca6351, 0x14292967, 
+        0x983e5152, 0xa831c66d, 0xb00327c8, 0xbf597fc7,
+        0xc6e00bf3, 0xd5a79147, 0x06ca6351, 0x14292967,
         0x27b70a85, 0x2e1b2138, 0x4d2c6dfc, 0x53380d13,
         0x650a7354, 0x766a0abb, 0x81c2c92e, 0x92722c85,
         0xa2bfe8a1, 0xa81a664b, 0xc24b8b70, 0xc76c51a3,
         0xd192e819, 0xd6990624, 0xf40e3585, 0x106aa070,
         0x19a4c116, 0x1e376c08, 0x2748774c, 0x34b0bcb5,
-        0x391c0cb3, 0x4ed8aa4a, 0x5b9cca4f, 0x682e6ff3, 
+        0x391c0cb3, 0x4ed8aa4a, 0x5b9cca4f, 0x682e6ff3,
         0x748f82ee, 0x78a5636f, 0x84c87814, 0x8cc70208,
         0x90befffa, 0xa4506ceb, 0xbef9a3f7, 0xc67178f2
     };
@@ -186,7 +186,7 @@ void calculateHash(FILE *file, bool verbose)
 
     // The current message block
 
-    // For loop to iterate through the message block 
+    // For loop to iterate through the message block
     int j;
 
     if (verbose) {
@@ -196,7 +196,7 @@ void calculateHash(FILE *file, bool verbose)
     while(fillMessageBlock(file, &msgBlock, &state, &numBits, verbose))
     {
         for(j=0; j<16; j++)
-        {   
+        {
             // Fist check for big or little endian
             // If our system is big endian we dont need to do any conversion
             if(endianCheck()==true)
@@ -209,7 +209,7 @@ void calculateHash(FILE *file, bool verbose)
                 // Convert to big endian first
                 W[j] = byteSwap32(msgBlock.t[j]);
             }
-           
+
         }
 
         for (j=16; j<64; j++)
@@ -256,9 +256,9 @@ void calculateHash(FILE *file, bool verbose)
         H[5] = f + H[5];
         H[6] = g + H[6];
         H[7] = h + H[7];
-    
+
     }// end while
-    
+
     // Print the results
     if (verbose) {
         printf("\n=================== HASH OUTPUT ==================================\n\n");
@@ -271,7 +271,7 @@ void calculateHash(FILE *file, bool verbose)
     printf("%08llx", (long long unsigned int) H[5]);
     printf("%08llx", (long long unsigned int) H[6]);
     printf("%08llx\n", (long long unsigned int) H[7]);
-    
+
     if (verbose) {
         printf("\n==================================================================\n\n");
     }
@@ -281,7 +281,7 @@ void calculateHash(FILE *file, bool verbose)
 
 // This function is used to handle the opening and reading of files
 int fillMessageBlock(FILE *file, union messageBlock *msgBlock, enum status *state, __uint64_t *numBits, bool verbose)
-{   
+{
     // Variables
     __uint64_t numBytes;
     int i;
@@ -329,10 +329,10 @@ int fillMessageBlock(FILE *file, union messageBlock *msgBlock, enum status *stat
     // Read bytes instead of characters
     // Read until the end of the file
     numBytes = fread(msgBlock->e, 1, 64, file);
-    
+
     // Keep track of the number of bytes we've read
     *numBits = *numBits + (numBytes * 8);
-    
+
     // If theres enough room to finish the padding
     if(numBytes < 56)
     {
@@ -345,7 +345,7 @@ int fillMessageBlock(FILE *file, union messageBlock *msgBlock, enum status *stat
         {
             // Add the index into our block
             numBytes = numBytes +1;
-            
+
             // Add enough zeroes so that there are 64 bits left at the end
             msgBlock->e[numBytes] = 0x00;
         }
@@ -358,10 +358,10 @@ int fillMessageBlock(FILE *file, union messageBlock *msgBlock, enum status *stat
     }
     // Otherwise, check if we can put some padding into this message block
     else if(numBytes < 64)
-    {   
+    {
         // Set the state to PAD0
         *state = PAD0;
-        
+
         // 0x80 = 10000000
         // Add the one bit into the current message block
         msgBlock->e[numBytes] = 0x80;
@@ -381,7 +381,7 @@ int fillMessageBlock(FILE *file, union messageBlock *msgBlock, enum status *stat
         // We need a message Block full of padding
         *state = PAD1;
     }
-    
+
     // Print padding
     /*
     printf("\n--- PADDING --- \n");
@@ -393,7 +393,7 @@ int fillMessageBlock(FILE *file, union messageBlock *msgBlock, enum status *stat
     */
     return 1;
 }
-    
+
 // This function is used to read the contents of the file and return them as an array of chars
 void printFileContents(FILE *fileForPrinting)
 {
@@ -420,15 +420,15 @@ void printFileContents(FILE *fileForPrinting)
             // Print the contents of the file
             printf(" %s\n", fileContents);
         };
-        
+
         printf("\n ========================================= \n");
 
         fclose(fileForPrinting);
 
-        // Close the file 
+        // Close the file
         return;
     }
-    
+
 }
 // Simple function that calcuates the size of a file
 int calcFileSize(FILE *file)
@@ -436,7 +436,7 @@ int calcFileSize(FILE *file)
     int prev=ftell(file);
     fseek(file, 0L, SEEK_END);
     int size=ftell(file);
-    fseek(file,prev,SEEK_SET); 
+    fseek(file,prev,SEEK_SET);
     return size;
 }
 
@@ -460,8 +460,8 @@ _Bool endianCheck()
         }
 }
 
-// Section 4.1.2  
-// ROTR = Rotate Right 
+// Section 4.1.2
+// ROTR = Rotate Right
 // SHR = Shift Right
 // ROTR_n(x) = (x >> n) | (x << (32-n))
 // SHR_n(x) = (x >> n)
